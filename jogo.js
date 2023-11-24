a9f414aa9254852f46de449bda87c5737ec7c5db
nathalhavitoriakkk <106328684+nathalhavitoriakkk@users.noreply.github.com>
2023-11-24 10:17:42 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,27 @@
+const celulas = document.querySelector(".celula");
+let checarTurno = true;
+let turno;
+
+//Define constantes para representar os jogadores "X" e "Y"
+const JOGADOR_X = "X";
+const JOGADOR_O = "O";
+
+let jogoAcabou = false;
+
+document.addEventListener("click", (event) => {
+    if (event.target.matches(".celula") && !jogoAcabou) {
+        jogar(event.target.id);
+    }
+});
+function jogar(id) {
+    const celula = document.getElementById(id);
+    if (celula.textContent === "") {
+        turno = checarTurno ? JOGADOR_X : JOGADOR_O;
+        checarTurno = !checarTurno;
+        celula.textContent = turno;
+        celula.classList.add(turno);
+        celula.removeEventListener("click", jogar);
+        verificarVencedor(turno);
+
+    }
+}
